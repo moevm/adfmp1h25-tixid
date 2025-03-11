a7e6563b94b85210395f24f5c2158cd413834ed3
Artur Musaev <90715546+arturmusaev1@users.noreply.github.com>
2025-03-12 00:44:07 +0300

    Add files via upload

---
+++ tixid/app/src/main/java/com/tixid/tixid/GameMapActivity.kt
@@ -0,0 +1,13 @@
+package com.tixid.tixid
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class GameMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_map)
+    }
+}
