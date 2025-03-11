c502b703b07a5ffb8076b9af3c452e4fe9f30461
evildre <musaew97@gmail.com>
2025-03-11 14:32:56 +0300

    add creators

--- tixid/app/src/main/java/com/tixid/tixid/MainActivity.kt
+++ tixid/app/src/main/java/com/tixid/tixid/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.tixid.tixid
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnStatistics = findViewById<Button>(R.id.btn_statistics)
+        val btnCreators = findViewById<Button>(R.id.btn_creators)
+
+        btnCreators.setOnClickListener {
+            val intent = Intent(this, CreatorsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
